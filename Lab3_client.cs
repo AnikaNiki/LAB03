8b7d331ec7d02a1b6d97eb6febbd333cfb532fcc
Anna Nikiforova <153638434+AnikaNiki@users.noreply.github.com>
2024-04-06 00:15:34 +0300

    Add files via upload

---
+++ Lab3_client.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab3_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            try
+            {
+                client.Connect("127.0.0.1", 8888);
+
+                NetworkStream stream = client.GetStream();
+
+                Console.Write("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): > ");
+                string command = Console.ReadLine();
+                string message;
+
+                if (command == "exit")
+                {
+                    message = $"{command}";
+                    byte[] data1 = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data1, 0, data1.Length);
+                    Console.WriteLine("The request was sent.");
+
+                    // Закрываем соединение
+                    stream.Close();
+                    client.Close();
+                    return;
+                }
+
+
+                Console.Write("Enter filename: > ");
+                string name = Console.ReadLine();
+
+
+                message = $"{command}\n{name}";
+                if (command == "2")
+                {
+                    Console.Write("Enter the content: > ");
+                    string content = Console.ReadLine();
+                    message += $"\n{content}";
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("The request was sent.");
+
+
+                // Получение ответа от сервера
+                byte[] responseData = new byte[256];
+                int bytes = stream.Read(responseData, 0, responseData.Length);
+                string response = Encoding.UTF8.GetString(responseData, 0, bytes);
+                switch (command)
+                {
+                    case "1":
+
+                        string[] requestData = response.Split('\n');
+                        if (requestData[0] == "200") { Console.WriteLine("The content of the file is: " + requestData[1]); }
+                        else { Console.WriteLine("The response says that the file was not found!"); }
+                        break;
+
+                    case "2":
+
+                        if (response == "200") { Console.WriteLine("The response says that the file was created!"); }
+                        else { Console.WriteLine("The response says that creating the file was forbidden!"); }
+                        break;
+
+                    case "3":
+
+                        if (response == "200") { Console.WriteLine("The response says that the file was successfully deleted!"); }
+                        else { Console.WriteLine("The response says that the file was not found!"); }
+                        break;
+
+                }
+
+
+
+                stream.Close();
+                // Закрываем соединение
+                client.Close();
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab3_server.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"C:\Users\Anika\Documents\ПИ\server\data";
+
+
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(localAddr, 8888);
+            server.Start();    // запускаем сервер
+            Console.WriteLine("Server started!");
+
+            while (true)
+            {
+                // получаем подключение в виде TcpClient
+                TcpClient client = server.AcceptTcpClient();
+
+                // получение данных от пользователя
+                NetworkStream stream = client.GetStream();
+
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                string messenger = Encoding.UTF8.GetString(data, 0, bytes);
+
+                string[] requestData = messenger.Split('\n');
+
+                if (requestData[0] == "exit")
+                {
+                    stream.Close();
+                    server.Stop(); // останавливаем сервер
+
+                    return;
+                }
+
+                string number = requestData[0];
+                string name = requestData[1];
+
+                string filePath = Path.Combine(folderPath, name);
+                string response = "";
+                switch (number)
+                {
+                    case "1":
+                        try
+                        {
+                            // Проверяем существование файла
+                            if (File.Exists(filePath))
+                            {
+                                // Читаем все строки из файла
+                                string lines = File.ReadAllText(filePath);
+                                response = "200\n" + lines;
+                                Console.WriteLine("200");
+                            }
+                            else
+                            {
+                                response = "403\n ";
+                                Console.WriteLine("403");
+                            }
+                        }
+                        catch
+                        {
+                            response = "403\n ";
+                            Console.WriteLine("403");
+                        }
+                        break;
+
+                    case "2":
+                        try
+                        {
+                            if (File.Exists(filePath))
+                            {
+                                response = "403";
+                                Console.WriteLine("403");
+                            }
+                            else
+                            {
+                                string content = requestData[2];
+                                File.WriteAllText(filePath, content);
+
+                                response = "200";
+                                Console.WriteLine("200");
+
+                            }
+                        }
+                        catch
+                        {
+                            response = "403\n ";
+                            Console.WriteLine("403");
+                        }
+
+                        break;
+
+                    case "3":
+                        try
+                        {
+                            // Проверяем существование файла
+                            if (File.Exists(filePath))
+                            {
+                                // Удаляем файл
+                                File.Delete(filePath);
+                                response = "200";
+                                Console.WriteLine("200");
+                            }
+                            else
+                            {
+                                response = "403";
+                                Console.WriteLine("403");
+                            }
+                        }
+                        catch
+                        {
+                            response = "403";
+                            Console.WriteLine("403");
+                        }
+                        break;
+                }
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseData, 0, responseData.Length);
+            }
+        }
+    }
+}
